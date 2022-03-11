6282e17f7f7485c45529b9d72ca52ef3af4c6ed1
MasakiNAGAO <masaki.nagao98@gmail.com>
2022-03-11 00:00:44 +0000

    php artisan serve dosent work

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Category;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::create(
+            [
+            'name' => '',
+            'headline' => '',
+            'caption' => "",
+            'major_category_name' => '',
+            'major_category_id' => 
+            ],
+            [
+            'name' => '',
+            'headline' => '',
+            'caption' => "",
+            'major_category_name' => '',
+            'major_category_id' => 
+            ],
+            [
+            'name' => '',
+            'headline' => '',
+            'caption' => "",
+            'major_category_name' => '',
+            'major_category_id' => 
+            ],
+            [
+            'name' => '',
+            'headline' => '',
+            'caption' => "",
+            'major_category_name' => '',
+            'major_category_id' => 
+            ],
+            [
+            'name' => '',
+            'headline' => '',
+            'caption' => "",
+            'major_category_name' => '',
+            'major_category_id' => 
+            ],
+            [
+            'name' => '',
+            'headline' => '',
+            'caption' => "",
+            'major_category_name' => '',
+            'major_category_id' => 
+            ]
+        );
+        }
+    }
+}
